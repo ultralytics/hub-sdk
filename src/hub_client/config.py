c460c7f27763e9540fb9e3f349a5d6acb4641f62
Hassaan Dar <hassaan.dar@ultralytics.com>
2023-08-16 16:09:34 +0500

    feat(env):  env added HUB_FUNCTIONS_ROOT

--- src/hub_client/config.py
+++ src/hub_client/config.py
@@ -2,3 +2,4 @@ import os
 
 HUB_API_ROOT = os.environ.get('ULTRALYTICS_HUB_API', 'https://api.ultralytics.com')
 HUB_WEB_ROOT = os.environ.get('ULTRALYTICS_HUB_WEB', 'https://hub.ultralytics.com')
+HUB_FUNCTIONS_ROOT = os.environ.get('ULTRALYTICS_HUB_FUNCTIONS', 'https://europe-west1-ultralytics-hub-staging.cloudfunctions.net/')
