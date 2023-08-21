02d660cc5ae2029b784515cfa2ebb5d922714f82
Hassaan Dar <hassaan.dar@ultralytics.com>
2023-08-21 20:16:00 +0500

    feat(Docstring): Adding docstring for all code

--- src/hub_client/models.py
+++ src/hub_client/models.py
@@ -3,10 +3,30 @@ from .crud_client import CRUDClient
 
 class Models(CRUDClient):
     def __init__(self, headers=None):
+        """
+        Initialize a Models instance.
+
+        Args:
+            headers (dict, optional): Headers to be included in API requests. Defaults to None.
+        """
         super().__init__("models", "model", headers)
 
 
     def cleanup(self, id):
+        """
+        Delete a model resource by its ID.
+
+        This method sends a DELETE request to the API to delete the model resource with the specified ID.
+
+        Args:
+            id (int): The ID of the model resource to be deleted.
+
+        Returns:
+            dict: A dictionary containing the API response data after the deletion.
+
+        Raises:
+            Exception: If an error occurs during the deletion process.
+        """
         try:
             return self._handle_request(self.api_client.delete, f"/{id}")
         except Exception as e:
