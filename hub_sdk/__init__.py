9ee0e1cbb0990f6a9a15c7bbea173c42b1adf574
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-12-29 05:40:09 -0500

    feat: replace `setup.py` with `pyproject.toml` (#57)
    Co-authored-by: Kalen Michael <kalenmike@gmail.com>

--- hub_sdk/__init__.py
+++ hub_sdk/__init__.py
@@ -3,5 +3,5 @@
 from hub_sdk.config import HUB_API_ROOT, HUB_WEB_ROOT
 from hub_sdk.hub_client import HUBClient
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __all__ = '__version__', 'HUBClient', 'HUB_API_ROOT', 'HUB_WEB_ROOT'
