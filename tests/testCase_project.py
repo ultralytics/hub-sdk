d702db9b35ae96c9512f4436cf127ed9be76f8b1
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-02-21 14:06:09 +0100

    Add docstrings to tests

--- tests/testCase_project.py
+++ tests/testCase_project.py
@@ -5,12 +5,14 @@ from hub_sdk import HUBClient
 
 class TestProjectSDK(unittest.TestCase):
     def setUp(self):
+        """Initializes test environment, instances of HUBClient before each test."""
         self.client = HUBClient({"email": "<Email>", "password": "<Password>"})  # Add Email Password
         self.project = self.client.project()
         self.assertIsNotNone(self.client)
         self.assertIsNotNone(self.project)
 
     def test_create_project(self):
+        """Tests project creation using HUBClient."""
         data = {"meta": {"name": "my project"}}
         expected_result = None
         result = self.project.create_project(data)
@@ -18,11 +20,13 @@ class TestProjectSDK(unittest.TestCase):
         self.assertEqual(result.data.get("meta").get("name"), "my project")
 
     def test_get_project_by_id(self):
+        """Tests retrieval of a project by its ID using HUBClient."""
         project_id = "<Project ID>"  # Add Project ID
         result = self.client.project(project_id)
         self.assertEqual(result.id, project_id)
 
     def test_update_project(self):
+        """Tests updating a project's details using HUBClient."""
         data = {"meta": {"name": "Project name update"}}
         project_id = "<Project ID>"  # Add Project ID
         result = self.client.project(project_id)
@@ -31,16 +35,19 @@ class TestProjectSDK(unittest.TestCase):
         self.assertEqual(result.data.get("meta").get("name"), expected_result)
 
     def test_list_projects(self):
+        """Tests listing of all projects using HUBClient."""
         result = self.client.project_list(page_size=1, public=True)
         self.assertNotEqual(len(result), 0)
 
     def test_upload_image(self):
+        """Tests image upload functionality using HUBClient."""
         project_id = "<Project ID>"  # Add Project ID
         result = self.client.project(project_id)
         result.upload_image(file="project_image.jpeg")
         self.assertEqual(result.id, project_id)
 
     def test_delete_project(self):
+        """Tests deletion of a project using HUBClient."""
         project_id = "<Project ID>"  # Add Project ID
         result = self.client.project(project_id)
         deleted = result.delete()
