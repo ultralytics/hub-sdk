d8d05cccbdb93b95684c2710bede41e7f8f4022d
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-01-15 13:23:46 +0100

    v0.0.18 with improved file headers
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- hub_sdk/__init__.py
+++ hub_sdk/__init__.py
@@ -3,5 +3,5 @@
 from hub_sdk.config import HUB_API_ROOT, HUB_WEB_ROOT
 from hub_sdk.hub_client import HUBClient
 
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 __all__ = "__version__", "HUBClient", "HUB_API_ROOT", "HUB_WEB_ROOT"
