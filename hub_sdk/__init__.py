104c35242e4717c192377c2f712aa1380328a2c2
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-09-03 15:23:52 +0200

    Update __init__.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- hub_sdk/__init__.py
+++ hub_sdk/__init__.py
@@ -3,5 +3,5 @@
 from hub_sdk.config import HUB_API_ROOT, HUB_WEB_ROOT
 from hub_sdk.hub_client import HUBClient
 
-__version__ = "0.0.23"
+__version__ = "0.0.24"
 __all__ = "__version__", "HUBClient", "HUB_API_ROOT", "HUB_WEB_ROOT"
