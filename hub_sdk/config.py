839dde8054453f0b6432f8ae5cd0a783904832f7
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-04-24 12:02:09 +0200

    Remove hardcoded Firebase API key

--- hub_sdk/config.py
+++ hub_sdk/config.py
@@ -4,10 +4,10 @@ import os
 
 HUB_API_ROOT = os.environ.get("ULTRALYTICS_HUB_API", "https://api.ultralytics.com")
 HUB_WEB_ROOT = os.environ.get("ULTRALYTICS_HUB_WEB", "https://hub.ultralytics.com")
+FIREBASE_API_KEY = os.environ.get("ULTRALYTICS_FIREBASE_API_KEY", "FIREBASE_API_KEY")
 FIREBASE_AUTH_URL = os.environ.get(
     "ULTRALYTICS_FIREBASE_AUTH_URL",
-    "http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDlTep"
-    "-ubgWoafviJJneFL35raoJjWFnOw",
+    f"http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_API_KEY}",
 )
 HUB_FUNCTIONS_ROOT = f"{HUB_API_ROOT}"
 
